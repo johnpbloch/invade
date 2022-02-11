d016b3ca10e30285da53ed48a85c1e9bef13b3d1
John P. Bloch <johnpbloch@users.noreply.github.com>
2022-02-11 10:21:24 -0600

    Add actual tests

--- tests/cases/InvadeTest.php
+++ tests/cases/InvadeTest.php
@@ -1,11 +1,42 @@
 <?php
 
+use JohnPBloch\Invade\Dummy;
 use PHPUnit\Framework\TestCase;
 
 class InvadeTest extends TestCase
 {
+    /**
+     * @covers invade
+     */
     public function testPropertyAccess()
     {
-        $this->markTestIncomplete();
+        $dummy = new Dummy();
+        $this->assertEquals('Foobar', invade($dummy)->test);
+        $this->assertEquals(0, invade($dummy)->count);
+    }
+
+    /**
+     * @covers invade
+     */
+    public function testPropertySetting()
+    {
+        $dummy = new Dummy();
+        invade($dummy)->test = 'Qwerty';
+        $this->assertEquals('Qwerty', invade($dummy)->test);
+        $val = rand(1, 99);
+        invade($dummy)->count = $val;
+        $this->assertEquals($val, invade($dummy)->count);
+    }
+
+    /**
+     * @covers invade
+     */
+    public function testMethodAccess()
+    {
+        $dummy = new Dummy();
+        invade($dummy)->transformTest();
+        $this->assertEquals('rabooF', invade($dummy)->test);
+        $this->assertEquals(1, invade($dummy)->incrementCount());
+        $this->assertEquals(1, invade($dummy)->count);
     }
 }
